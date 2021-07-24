26c2cd67d561630cc28599c26c010ad286aab07a
Tom Wilson <twilson63@gmail.com>
2021-07-24 17:12:29 +0000

    feat: successfully running harness (#5)

--- test/hyper.js
+++ test/hyper.js
@@ -1,11 +1,11 @@
 import { appOpine, core } from "../dev_deps.js";
-import myAdapter from "../mod.js";
+import mongo from "../mod.js";
 import PORT_NAME from "../port_name.js";
 
 const hyperConfig = {
   app: appOpine,
   adapters: [
-    { port: PORT_NAME, plugins: [myAdapter("mongodb://127.0.0.1:27017")] },
+    { port: PORT_NAME, plugins: [mongo("mongodb://127.0.0.1:27017")] },
   ],
 };
 
