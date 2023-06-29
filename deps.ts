50c50cf098e824a6f5552dc314086195749546da
Tyler Hall <tyler@hyper.io>
2023-06-29 19:57:16 +0000

    fix: generate actual name for database on create and store on meta doc #42

--- deps.ts
+++ deps.ts
@@ -5,8 +5,9 @@
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
 export * as R from 'https://cdn.skypack.dev/ramda@^0.29.0?dts'
 
-export { EJSON } from 'npm:bson'
-export { type Collection, MongoClient } from 'npm:mongodb'
+export { EJSON } from 'npm:bson@5.3.0'
+export { type Collection, MongoClient } from 'npm:mongodb@5.6.0'
+export { default as cuid } from 'npm:cuid@3.0.0'
 
 export {
   HyperErr,
