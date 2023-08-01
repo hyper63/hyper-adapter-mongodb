b5d2a4a5bf977f43c1af8d2dc0eb86ee6ef61d1a
Tyler Hall <tyler@hyper.io>
2023-08-01 20:16:49 +0000

    chore: bump deps and fix task

--- test/hyper.ts
+++ test/hyper.ts
@@ -1,6 +1,6 @@
 // Harness deps
-import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.1.0/packages/core/mod.ts'
-import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.1.0/packages/app-express/mod.ts'
+import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.2.0/packages/core/mod.ts'
+import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.0/packages/app-express/mod.ts'
 
 import mongo from '../mod.ts'
 import PORT_NAME from '../port_name.ts'
