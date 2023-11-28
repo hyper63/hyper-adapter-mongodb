f5e5e6bfa2ae68fd5d76d2ad7f8fdd288754fc27
Tyler Hall <tyler@hyper.io>
2023-11-28 18:52:17 -0500

    chore: bump deps and use npm in lieu of skypack

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,12 +1,12 @@
-export { default as pluginFactory } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.1.0/packages/core/utils/plugin-schema.ts'
-export { data as dataPort } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-port-data%40v2.1.0/packages/port-data/mod.ts'
+export { default as pluginFactory } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.1/packages/core/utils/plugin-schema.ts'
+export { data as dataPort } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-port-data%40v2.3.0/packages/port-data/mod.ts'
 
 // std lib deps
-export { deferred } from 'https://deno.land/std@0.190.0/async/deferred.ts'
+export { deferred } from 'https://deno.land/std@0.208.0/async/deferred.ts'
 export {
   assert,
   assertEquals,
   assertObjectMatch,
   assertRejects,
   assertThrows,
-} from 'https://deno.land/std@0.190.0/testing/asserts.ts'
+} from 'https://deno.land/std@0.208.0/assert/mod.ts'
