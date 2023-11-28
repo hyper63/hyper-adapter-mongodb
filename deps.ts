f463ffdd232e6b0d41d0682232872d123a55032a
Tyler Hall <tyler@hyper.io>
2023-11-28 18:52:17 -0500

    chore: bump deps and use npm in lieu of skypack

--- deps.ts
+++ deps.ts
@@ -3,13 +3,22 @@
  */
 // @deno-types="./crocks.d.ts"
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
-export * as R from 'https://cdn.skypack.dev/ramda@0.29.0?dts'
+// @deno-types="npm:@types/ramda@^0.29.9"
+export * as R from 'npm:ramda@0.29.1'
 
 export { EJSON } from 'npm:bson@6.2.0'
-export { type Collection, MongoClient } from 'npm:mongodb@6.2.0'
+export { type Collection, MongoClient } from 'npm:mongodb@6.3.0'
 export { default as cuid } from 'npm:cuid@3.0.0'
 
-export {
+import {
   HyperErr,
-  isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
+  isHyperErr as isHyperErrBase,
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.2/packages/utils/hyper-err.js'
+
+export { HyperErr }
+/**
+ * The new ramda types in hyper-utils are overly assuming, so
+ * just wrap the isHyperErr from utils with a more unassuming signature
+ */
+// deno-lint-ignore no-explicit-any
+export const isHyperErr = (v: any) => isHyperErrBase(v)
